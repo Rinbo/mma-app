9b1c29e6efa3efdeb95d074555a6af4380a74115
rinbo <robin.b@outlook.com>
2018-10-29 22:29:07 +0100

    Add Post request in Boxy components HandleFormSubmit method to update database with whatever new figher is passed into the forms add AddNewFighter method in body Component that also updates state with new fighter

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -6,17 +6,34 @@ class Body extends React.Component {
       fighters: []
     };
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.addNewFighter = this.addNewFighter.bind(this)
   }
   
   handleFormSubmit(name, description){
-    console.log(name, description)
+    let body = JSON.stringify({fighter: {name: name, description:   description} })
+    fetch('http://localhost:3000/api/v1/fighters', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: body,
+      }).then((response) => {return response.json()})
+      .then((fighter) => {
+        this.addNewFighter(fighter)
+      })    
+  }
+  
+  addNewFighter(fighter){
+    this.setState({
+      fighter: this.state.fighters.concat(fighter)
+    })
   }
 
   componentDidMount(){
     fetch('/api/v1/fighters.json')
       .then((response) => {return response.json()})
       .then((data) => {this.setState({ fighters: data }) });
-  }
+  }  
 
   render(){
     return(
