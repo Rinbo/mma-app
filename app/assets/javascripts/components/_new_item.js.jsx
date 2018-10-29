f8f728ab313d7fa7b5965e3d9207b03d5be6219d
rinbo <robin.b@outlook.com>
2018-10-29 22:19:01 +0100

    Add event listener on form that passes in form name and description to handleFromSubmit that is passsed to component as a prop from Body

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -2,7 +2,10 @@ const Newfighter = (props) => {
   let formFields = {}
  
   return(
-    <form>
+    <form onSubmit={ (e) => { 
+      props.handleFormSubmit(formFields.name.value, formFields.description.value); 
+      e.target.reset();
+      } }>
      <input ref={input => formFields.name = input} placeholder='Enter the name of the fighter'/>
      <input ref={input => formFields.description = input} placeholder='Enter a description' />
      <button>Submit</button>
