d4e37caa4660ce88cc67f15b49d731416b4fb461
rinbo <robin.b@outlook.com>
2018-10-29 23:16:25 +0100

    add rspec test to test Fighter model

---
+++ spec/models/fighter_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Fighter, type: :model do
+  describe 'should contain the following columns' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :description }    
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
+  end
+
+end
